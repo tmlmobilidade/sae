c1e65d2678cefd63a8ebf5c9d6ec7a56418249f6
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-11-27 12:15:00 +0000

    Removed Alerts Context

--- alerts/apps/frontend/src/providers/dashboard-providers.tsx
+++ alerts/apps/frontend/src/providers/dashboard-providers.tsx
@@ -1,12 +1,6 @@
 /* * */
 
-import { AlertsListContextProvider } from '@/contexts/AlertsList.context';
-
 /* * */
 export function DashboardProviders({ children }) {
-	return (
-		<AlertsListContextProvider>
-			{children}
-		</AlertsListContextProvider>
-	);
+	return children;
 }
