45633719f722d76e5f8cb84492961ff7056c0626
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-12-10 17:35:27 +0000

    Update sync tasks to use earliest data date from environment variable

--- sla/apps/sync-apex-t11/src/tasks/sync-apex-t11.ts
+++ sla/apps/sync-apex-t11/src/tasks/sync-apex-t11.ts
@@ -38,10 +38,10 @@ export async function syncApexT11() {
 		// It makes sense to divide chunks by day, but this should be adjusted according to the volume of data in each chunk.
 
 		const thirtySecondsAgo = DateTime.now().minus({ seconds: 30 });
-		const oldestDataNeeded = DateTime.fromFormat('20240801', 'yyyyMMdd').set({ hour: 4, minute: 0, second: 0 });
+		const earliestDataNeeded = DateTime.fromFormat(process.env.SYNC_EARLIEST_DATE, 'yyyyMMdd').set({ hour: 4, minute: 0, second: 0 });
 
 		const allTimestampChunks = Interval
-			.fromDateTimes(oldestDataNeeded, thirtySecondsAgo)
+			.fromDateTimes(earliestDataNeeded, thirtySecondsAgo)
 			.splitBy({ hour: 3 })
 			.sort((a, b) => b.start.toMillis() - a.start.toMillis());
 
