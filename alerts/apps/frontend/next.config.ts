43e00ac6d00617e559debef5e1b519588797a482
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-12-17 12:27:54 +0000

    Refactor Dockerfile for alerts apps

--- alerts/apps/frontend/next.config.ts
+++ alerts/apps/frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	output: 'standalone',
 	env: {
 		API_ALERTS_URL: process.env.API_ALERTS_URL,
 	},
