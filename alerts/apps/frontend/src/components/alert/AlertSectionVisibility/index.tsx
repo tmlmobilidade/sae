cc847afb61361f895b285e78d1f9b121f3b52291
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-01-24 18:18:41 +0000

    Refactor AlertSectionTitle and AlertSectionVisibility components

--- alerts/apps/frontend/src/components/alert/AlertSectionVisibility/index.tsx
+++ alerts/apps/frontend/src/components/alert/AlertSectionVisibility/index.tsx
@@ -1,28 +1,11 @@
-import { Section, Surface, TextArea, TextInput } from '@tmlmobilidade/ui';
+import { Section, Surface } from '@tmlmobilidade/ui';
 
 export default function AlertSectionVisibility() {
 	return (
-		<Section
-			title="Título e Descrição"
-			description="Breve descrição do que motivou a criação do alerta"
-		>
-			<Surface padding="sm" gap="md">
-				<TextInput
-					placeholder="..."
-					description="É importante que o título seja curto e claro, para que não apareça cortado no site, apps, etc."
-					label="Título Curto"
-				/>
-				<TextArea
-					placeholder="..."
-					description="Um bom alerta explica a situação de forma breve e clara, explicita as suas causas e como está a ser mitigado, e apresenta uma ou mais soluções de como o passageiro poderá ultrapassar esta situação."
-					label="Descrição"
-				/>
-				<TextInput
-					placeholder="https://www.cm-setubal.com/..."
-					description="Opcionalmente inclua o URL de um website onde é possivel obter mais informação"
-					label="Link Adicional"
-				/>
-			</Surface>
-		</Section>
+		<Section title="Visibilidade e Agendamento" description="É possível agendar a permanência do alerta nos canais digitais. A visibilidade do alerta é diferente do seu período de vigência.">
+		<Surface padding="sm" gap="md">
+			something
+		</Surface>
+	</Section>
 	);
 }
