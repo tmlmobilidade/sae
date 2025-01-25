c56a4d0d9ab9d5ee98e4d2042a1dc49b7f7a0061
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-01-25 12:57:51 +0000

    Refactor Visibility and Validity AlertForm Cards to include DateTimePicker

--- alerts/apps/frontend/src/components/alert/AlertSectionValidity/index.tsx
+++ alerts/apps/frontend/src/components/alert/AlertSectionValidity/index.tsx
@@ -1,10 +1,12 @@
-import { Section, Surface } from '@tmlmobilidade/ui';
+import { DateTimePicker, Section, Surface } from '@tmlmobilidade/ui';
+import styles from './styles.module.css';
 
 export default function AlertSectionValidity() {
 	return (
 		<Section title="Período de Vigência" description="Período em que o alerta é válido. Distinto da visibilidade. O alerta pode estar visível mas não ser ainda válido (ex: um alerta para um corte de estrada é vísível uma semana antes, mas o corte em si é apenas durante 2 dias).">
-		<Surface padding="sm" gap="md">
-			something
+		<Surface flexDirection="row" padding="sm" gap="md">
+			<DateTimePicker className={styles.datePicker} label="Data de Início" description="Data de início do alerta" />
+			<DateTimePicker className={styles.datePicker} label="Data de Fim" description="Data de fim do alerta" />
 		</Surface>
 	</Section>
 	);
--- alerts/apps/frontend/src/components/alert/AlertSectionVisibility/index.tsx
+++ alerts/apps/frontend/src/components/alert/AlertSectionVisibility/index.tsx
@@ -1,10 +1,12 @@
-import { Section, Surface } from '@tmlmobilidade/ui';
+import { DateTimePicker, Section, Surface } from '@tmlmobilidade/ui';
+import styles from './styles.module.css'
 
 export default function AlertSectionVisibility() {
 	return (
 		<Section title="Visibilidade e Agendamento" description="É possível agendar a permanência do alerta nos canais digitais. A visibilidade do alerta é diferente do seu período de vigência.">
-		<Surface padding="sm" gap="md">
-			something
+		<Surface flexDirection="row" padding="sm" gap="md">
+			<DateTimePicker className={styles.datePicker} label="Data de Início" description="Data de início do alerta" />
+			<DateTimePicker className={styles.datePicker} label="Data de Fim" description="Data de fim do alerta" />
 		</Surface>
 	</Section>
 	);
