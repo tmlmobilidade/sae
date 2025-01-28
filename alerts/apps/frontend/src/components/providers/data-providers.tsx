c1ebabd3206d0394e2ce2509cc1ee97045cfe708
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-01-28 14:18:47 +0000

    Refactor data providers and add LocationsContext

--- alerts/apps/frontend/src/components/providers/data-providers.tsx
+++ alerts/apps/frontend/src/components/providers/data-providers.tsx
@@ -3,16 +3,19 @@
 /* * */
 
 import { LinesContextProvider } from '@/contexts/Lines.context';
+import { LocationsContextProvider } from '@/contexts/Locations.context';
 import { StopsContextProvider } from '@/contexts/Stops.context';
 
 /* * */
 
 export function DataProviders({ children }: { children: React.ReactNode }) {
 	return (
-		<StopsContextProvider>
-			<LinesContextProvider>
-				{children}
-			</LinesContextProvider>
-		</StopsContextProvider>
+		<LocationsContextProvider>
+			<StopsContextProvider>
+				<LinesContextProvider>
+					{children}
+				</LinesContextProvider>
+			</StopsContextProvider>
+		</LocationsContextProvider>
 	);
 }
