57118e44c78adf4890ccbb4e70790ef57b42d1d4
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-01-28 16:33:38 +0000

    Refactor @tmlmobilidade/ui package version in package.json and pnpm-lock.yaml

--- alerts/apps/frontend/src/components/alert/AlertSectionReferences/index.tsx
+++ alerts/apps/frontend/src/components/alert/AlertSectionReferences/index.tsx
@@ -1,33 +1,35 @@
+'use client';
+
+/* * */
+
 import { Combobox, Section, SegmentedControl, Surface } from '@tmlmobilidade/ui';
 import styles from './styles.module.css';
 import ReferenceCard from '../ReferenceCard';
+import { useMemo } from 'react';
+import { useLocationsContext } from '@/contexts/Locations.context';
 
 export default function AlertSectionReferences() {
 
-	const municipalities = [
-		"Alcochete",
-		"Almada",
-		"Barreiro",
-		"Amadora",
-		"Cascais",
-		"Lisboa",
-		"Loures",
-		"Mafra",
-		"Moita",
-		"Montijo",
-		"Odivelas",
-		"Oeiras",
-		"Palmela",
-		"Seixal",
-		"Sintra",
-		"Sesimbra",
-		"Setúbal e Vila Franca de Xira"
-	]
+	//
+	// A. Setup variables
+	const { data } = useLocationsContext();
+
+	const municipalitiesOptions = useMemo(() => {
+		if (!data.municipalities) return [];
+	
+			return data.municipalities.map(municipality => ({
+				label: municipality.name,	
+				value: municipality.id,
+			}));
+	}, [data.municipalities]);
+
+	//
+	// B. Render components
 
 	return (
 		<Section title="Referências" description="As referências (Linhas, Paragens, Municípios, Etc...) afetadas deste alerta.">
 			<Surface padding="sm" gap="md">
-				<Combobox label="Municípios Afetados" description='Selecione os munícios que serão afetados pelo alerta' data={municipalities} searchable multiple fullWidth/>
+				<Combobox label="Municípios Afetados" description='Selecione os munícios que serão afetados pelo alerta' data={municipalitiesOptions} searchable multiple clearable fullWidth/>
 				<SegmentedControl data={[{label: 'Selecionar Linhas', value: 'lines'}, {label: 'Selecionar Paragens', value: 'stops'}, {label: 'Selecionar Agências', value: 'agencies'}]} fullWidth/>
 				<div className={styles.referenceCardContainer}>
 					<ReferenceCard />
