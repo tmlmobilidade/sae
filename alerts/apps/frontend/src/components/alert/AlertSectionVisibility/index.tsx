854eb15af11a7cc5cf947fc3ee0026e7a7e3f9ee
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-01-29 17:41:53 +0000

    Integrate alert detail context into AlertSectionVisibility for enhanced date input handling

--- alerts/apps/frontend/src/components/alert/AlertSectionVisibility/index.tsx
+++ alerts/apps/frontend/src/components/alert/AlertSectionVisibility/index.tsx
@@ -1,13 +1,31 @@
+'use client';
+
 import { DateTimePicker, Section, Surface } from '@tmlmobilidade/ui';
-import styles from './styles.module.css'
+import styles from './styles.module.css';
+import { useAlertDetailContext } from '@/contexts/AlertDetail.context';
 
 export default function AlertSectionVisibility() {
+	const { data: alertDetailData } = useAlertDetailContext();
+
 	return (
-		<Section title="Visibilidade e Agendamento" description="É possível agendar a permanência do alerta nos canais digitais. A visibilidade do alerta é diferente do seu período de vigência.">
-		<Surface flexDirection="row" padding="sm" gap="md">
-			<DateTimePicker className={styles.datePicker} label="Data de Início" description="Data de início do alerta" />
-			<DateTimePicker className={styles.datePicker} label="Data de Fim" description="Data de fim do alerta" />
-		</Surface>
-	</Section>
+		<Section
+			title="Visibilidade e Agendamento"
+			description="É possível agendar a permanência do alerta nos canais digitais. A visibilidade do alerta é diferente do seu período de vigência."
+		>
+			<Surface flexDirection="row" padding="sm" gap="md">
+				<DateTimePicker
+					className={styles.datePicker}
+					label="Data de Início"
+					description="Data de início do alerta"
+					{...alertDetailData.form.getInputProps('startDate')}
+				/>
+				<DateTimePicker
+					className={styles.datePicker}
+					label="Data de Fim"
+					description="Data de fim do alerta"
+					{...alertDetailData.form.getInputProps('endDate')}
+				/>
+			</Surface>
+		</Section>
 	);
 }
