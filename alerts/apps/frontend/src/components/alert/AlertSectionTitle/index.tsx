2103949f57622fcbbb5ab6cadb7cdd8fd146107d
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-01-29 17:41:47 +0000

    Integrate alert detail context into AlertSectionTitle for enhanced input handling

--- alerts/apps/frontend/src/components/alert/AlertSectionTitle/index.tsx
+++ alerts/apps/frontend/src/components/alert/AlertSectionTitle/index.tsx
@@ -1,14 +1,41 @@
-import { IconLink } from "@tabler/icons-react";
-import { Section, Surface, TextArea, TextInput } from "@tmlmobilidade/ui";
+'use client';
+
+import { IconLink } from '@tabler/icons-react';
+import { Section, Surface, TextArea, TextInput } from '@tmlmobilidade/ui';
+import { useAlertDetailContext } from '@/contexts/AlertDetail.context';
 
 export default function AlertSectionTitle() {
-    return (
-        <Section title="Título e Descrição" description="Breve descrição do que motivou a criação do alerta">
-            <Surface padding="sm" gap="md">
-                <TextInput withAsterisk placeholder="..." description="É importante que o título seja curto e claro, para que não apareça cortado no site, apps, etc." label="Título Curto" maxLength={255}/>
-                <TextArea withAsterisk placeholder="..." description="Um bom alerta explica a situação de forma breve e clara, explicita as suas causas e como está a ser mitigado, e apresenta uma ou mais soluções de como o passageiro poderá ultrapassar esta situação." label="Descrição" />
-                <TextInput leftSection={<IconLink size={18} />} placeholder="https://www.cm-setubal.com/..." description="Opcionalmente inclua o URL de um website onde é possivel obter mais informação" label="Link Adicional" />
-            </Surface>
-        </Section>
-    )
+	const { data: alertDetailData } = useAlertDetailContext();
+
+	return (
+		<Section
+			title="Título e Descrição"
+			description="Breve descrição do que motivou a criação do alerta"
+		>
+			<Surface padding="sm" gap="md">
+				<TextInput
+					withAsterisk
+					placeholder="..."
+					description="É importante que o título seja curto e claro, para que não apareça cortado no site, apps, etc."
+					label="Título Curto"
+					maxLength={255}
+					{...alertDetailData.form.getInputProps('title')}
+				/>
+				<TextArea
+					withAsterisk
+					placeholder="..."
+					description="Um bom alerta explica a situação de forma breve e clara, explicita as suas causas e como está a ser mitigado, e apresenta uma ou mais soluções de como o passageiro poderá ultrapassar esta situação."
+					label="Descrição"
+					{...alertDetailData.form.getInputProps('description')}
+				/>
+				<TextInput
+					leftSection={<IconLink size={18} />}
+					placeholder="https://www.cm-setubal.com/..."
+					description="Opcionalmente inclua o URL de um website onde é possivel obter mais informação"
+					label="Link Adicional"
+					{...alertDetailData.form.getInputProps('link')}
+				/>
+			</Surface>
+		</Section>
+	);
 }
