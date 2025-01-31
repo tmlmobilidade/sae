c56433a12c2f23c93cb23574b7a7bc9b89796871
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-01-31 18:27:55 +0000

    Add utility functions for alert line and stop retrieval

---
+++ alerts/apps/frontend/src/lib/alert-utils.ts
@@ -0,0 +1,25 @@
+import { Alert } from "@tmlmobilidade/core-types";
+
+export function getAvailableLines(alert: Alert) {
+    if (alert.reference_type === 'route') {
+        return alert.references.map(reference => reference.parent_id);
+    }
+
+    if (alert.reference_type === 'stop') {
+        return alert.references.flatMap(reference => reference.child_ids);
+    }
+
+    return [];
+}
+
+export function getAvailableStops(alert: Alert) {
+    if (alert.reference_type === 'route') {
+        return alert.references.flatMap(reference => reference.child_ids);
+    }
+
+    if (alert.reference_type === 'stop') {
+        return alert.references.map(reference => reference.parent_id);
+    }
+
+    return [];
+}
