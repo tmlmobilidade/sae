37272d38f31556e557832c340149992204f717d7
João de Vasconcelos <contact@joao.earth>
2025-02-04 19:55:46 +0000

    Set default operational status to 'scheduled' in createRidesFromGtfs function

--- sla/apps/feeder/src/tasks/createRidesFromGtfs.ts
+++ sla/apps/feeder/src/tasks/createRidesFromGtfs.ts
@@ -583,7 +583,7 @@ export async function createRidesFromGtfs() {
 								headsign: tripData.trip_headsign,
 								line_id: routeData.line_id,
 								operational_date: calendarDate,
-								operational_status: null,
+								operational_status: 'scheduled',
 								passengers_estimated: null,
 								pattern_id: tripData.pattern_id,
 								plan_id: planData._id,
