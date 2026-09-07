40e5c2868cd42347bb3d1b02b614a1aaf32ffdd8
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-07 17:56:54 +0100

    Add new agency mappings and enhance validation schema for transaction payloads
    * Added new agency mappings for Scotturb and Portal Viva in agency-map.ts to expand coverage of operators.
    * Updated validation schema in validation-v40.ts to include onBehalfOfOperatorLongID, greylistItemsCount, and profilesUsedCount, improving data validation and flexibility.

--- modules/apex/packages/parsers/src/agency-map.ts
+++ modules/apex/packages/parsers/src/agency-map.ts
@@ -6,9 +6,11 @@ const operatorLongIdToAgencyIdMap = new Map<string, string>([
 	['3', 'N18KL'], // Comboios de Portugal
 	['4', 'LTP61'], // Transtejo
 	['8', 'A3H3M'], // TCB
+	['10', 'ZVBA1'], // Scotturb
 	['15', '7NTB1'], // Fertagus
 	['16', 'KB1F6'], // MTS (Almada)
 	['21', 'HF16N'], // MobiCascais
+	['23', 'WCEDH'], // Portal Viva
 	['41', 'LA77N'], // Viação Alvorada (CM)
 	['42', 'BNA17'], // Rodoviária de Lisboa (CM)
 	['43', 'YA15B'], // TST (CM)
