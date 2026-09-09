35fd49a0b8522af5b0fbbeb5a7c2d2fd273fec27
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-09 22:52:19 +0100

    refactor(rides): update MongoDB indexes for improved query performance
    * Revised the ridesIndexes configuration to enhance query efficiency by adding new composite indexes involving agency_id and start_time_scheduled.
    * Removed redundant indexes and reorganized existing ones to streamline the indexing strategy, improving overall data retrieval performance.

--- packages-new/interfaces/godb/src/indexes/operation/rides.ts
+++ packages-new/interfaces/godb/src/indexes/operation/rides.ts
@@ -3,8 +3,6 @@
 import { type SimplifiedMongoIndex } from '@tmlmobilidade/go-clients-mongo';
 import { type Ride } from '@tmlmobilidade/go-types-operation';
 
-/* * */
-
 /**
  * **IMPORTANT**:
  * Automatic sorting (ESLint) of keys in the JS objects should be disabled.
@@ -12,20 +10,22 @@ import { type Ride } from '@tmlmobilidade/go-types-operation';
  * carefully considered based on the cardinality of each key.
  */
 export const ridesIndexes: SimplifiedMongoIndex<Ride>[] = [
-	{ key: { hashed_trip_id: 1 } },
-	{ key: { hashed_shape_id: 1 } },
-	{ key: { operational_date: 1 } },
-	{ key: { operational_date: 1, system_status: 1 } },
-	{ key: { start_time_scheduled: 1 } },
-	{ key: { system_status: 1 } },
-	// eslint-disable-next-line perfectionist/sort-objects
-	{ key: { system_status: 1, start_time_scheduled: 1 } },
-	// eslint-disable-next-line perfectionist/sort-objects
-	{ key: { trip_id: 1, start_time_scheduled: 1 } },
-	{ key: { plan_id: 1 } },
-	{ key: { shape_id: 1 } },
+	{ key: { agency_id: 1, hashed_trip_id: 1, start_time_scheduled: 1 } },
 	{ key: { agency_id: 1, route_short_name: 1, start_time_scheduled: 1 } },
+	{ key: { agency_id: 1, shape_id: 1, start_time_scheduled: 1 } },
 	// eslint-disable-next-line perfectionist/sort-objects
 	{ key: { agency_id: 1, start_time_scheduled: -1, hashed_trip_id: 1 } },
-	{ key: { agency_id: 1, hashed_trip_id: 1, start_time_scheduled: 1 } },
+	// eslint-disable-next-line perfectionist/sort-objects
+	{ key: { agency_id: 1, trip_id: 1, start_time_scheduled: 1 } },
+	{ key: { hash: 1 } },
+	{ key: { hashed_shape_id: 1 } },
+	{ key: { hashed_trip_id: 1 } },
+	{ key: { operational_date: 1 } },
+	{ key: { operational_date: 1, processing_status: 1 } },
+	{ key: { plan_id: 1 } },
+	{ key: { processing_status: 1 } },
+	{ key: { processing_status: 1, start_time_scheduled: 1 } },
+	{ key: { processing_status: 1, updated_at: 1 } },
+	{ key: { shape_id: 1 } },
+	{ key: { start_time_scheduled: 1 } },
 ];
