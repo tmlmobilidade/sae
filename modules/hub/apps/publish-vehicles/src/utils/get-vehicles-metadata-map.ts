745152412211c1539d526f58ed71039988155e49
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-10 18:17:07 +0100

    refactor(vehicles): update vehicle metadata retrieval and caching logic
    * Replaced the previous vehicle type with `HubV1ApiVehicleMetadata` for improved metadata handling.
    * Simplified the local cache implementation by directly using a Map for vehicle metadata storage.
    * Enhanced the `getVehiclesMetadataMap` function to validate and parse vehicle metadata using the new schema, ensuring data integrity.
    * Updated logging to reflect the new structure of the cached vehicle metadata.

--- modules/hub/apps/publish-vehicles/src/utils/get-vehicles-metadata-map.ts
+++ modules/hub/apps/publish-vehicles/src/utils/get-vehicles-metadata-map.ts
@@ -1,15 +1,15 @@
 /* * */
 
 import { goDb } from '@tmlmobilidade/go-interfaces-godb';
-import { type Vehicle } from '@tmlmobilidade/go-types-operation';
-import { UnixMilliseconds } from '@tmlmobilidade/go-types-shared';
+import { type HubV1ApiVehicleMetadata, HubV1ApiVehicleMetadataSchema } from '@tmlmobilidade/go-types-hub';
+import { type UnixMilliseconds } from '@tmlmobilidade/go-types-shared';
 import { Dates } from '@tmlmobilidade/go-utils-dates';
 import { Logger } from '@tmlmobilidade/logger';
 import { Timer } from '@tmlmobilidade/timer';
 
 /* * */
 
-let LOCAL_CACHE_DATA: Map<string, Vehicle> | null = null;
+let LOCAL_CACHE_DATA = new Map<string, HubV1ApiVehicleMetadata>();
 let LOCAL_CACHE_TIMESTAMP: null | UnixMilliseconds = null;
 
 /**
@@ -17,7 +17,7 @@ let LOCAL_CACHE_TIMESTAMP: null | UnixMilliseconds = null;
  * The local cache is updated every 10 minutes.
  * @returns A map of vehicle metadata by agency ID and vehicle ID.
  */
-export async function getVehiclesMetadataMap(): Promise<Map<string, Vehicle>> {
+export async function getVehiclesMetadataMap(): Promise<Map<string, HubV1ApiVehicleMetadata>> {
 	//
 
 	//
@@ -25,7 +25,7 @@ export async function getVehiclesMetadataMap(): Promise<Map<string, Vehicle>> {
 
 	const cacheIsExpired = LOCAL_CACHE_TIMESTAMP < Dates.now('utc').minus({ minutes: 10 }).unix_milliseconds;
 
-	if (!cacheIsExpired && LOCAL_CACHE_DATA && LOCAL_CACHE_DATA.size > 0) return LOCAL_CACHE_DATA;
+	if (!cacheIsExpired && LOCAL_CACHE_DATA.size > 0) return LOCAL_CACHE_DATA;
 
 	//
 	// Otherwise, retrieve fresh data from the database
@@ -36,12 +36,30 @@ export async function getVehiclesMetadataMap(): Promise<Map<string, Vehicle>> {
 
 	const foundVehicles = await goDb.operation.vehicles.findMany();
 
-	const vehiclesMetadataMap = new Map<string, Vehicle>(foundVehicles.map(vehicle => [`${vehicle.agency_id}:${vehicle.vehicle_id}`, vehicle]));
+	LOCAL_CACHE_DATA = new Map<string, HubV1ApiVehicleMetadata>();
 
-	LOCAL_CACHE_DATA = vehiclesMetadataMap;
+	foundVehicles.forEach((vehicle) => {
+		const metadata = HubV1ApiVehicleMetadataSchema.safeParse({
+			_id: vehicle._id,
+			agency_id: vehicle.agency_id,
+			available_seats: vehicle.available_seats,
+			contactless: vehicle.contactless,
+			license_plate: vehicle.license_plate,
+			make: vehicle.make,
+			model: vehicle.model,
+			propulsion: vehicle.propulsion,
+			registration_date: vehicle.registration_date,
+			vehicle_id: vehicle.vehicle_id,
+			vehicle_type: vehicle.typology,
+			wheelchair: vehicle.wheelchair,
+		});
+		if (!metadata.success) return;
+		LOCAL_CACHE_DATA.set(`${vehicle.agency_id}:${vehicle.vehicle_id}`, metadata.data);
+	});
+
 	LOCAL_CACHE_TIMESTAMP = Dates.now('utc').unix_milliseconds;
 
-	Logger.info({ message: `Retrieved ${vehiclesMetadataMap.size} vehicles metadata in ${timer.get()}` });
+	Logger.info({ message: `Retrieved ${LOCAL_CACHE_DATA.size} vehicles metadata in ${timer.get()}` });
 
-	return vehiclesMetadataMap;
+	return LOCAL_CACHE_DATA;
 };
